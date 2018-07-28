18cd8c90127cf877746039ad8b3012f36d492c1b
魏文洲 <>
2018-07-28 15:58:10 +1000

    finish country choose dialog

--- app/src/main/java/com/example/nortonwei/skycar/SignUpActivity.java
+++ app/src/main/java/com/example/nortonwei/skycar/SignUpActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.SpannableString;
@@ -16,11 +17,15 @@ import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class SignUpActivity extends AppCompatActivity {
+    int countryChoice = -1;
+    final String[] countryList = {"澳大利亚", "新西兰", "中国", "中国香港", "中国澳门", "中国台湾", "蒙古", "朝鲜", "韩国", "日本", "菲律宾", "越南", "老挝", "柬埔寨", "缅甸", "泰国", "马来西亚", "文莱", "新加坡", "印度尼西亚", "东帝汶"};
+    final String[] countryCodeList = {"+61", "+64", "+86", "+852", "+853", "+886", "+976", "+850", "+82", "+81", "+63", "+84", "+856", "+855", "+95", "+66", "+60", "+673", "+65", "+62", "+670"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -100,10 +105,29 @@ public class SignUpActivity extends AppCompatActivity {
         EditText countryCodeEditText = (EditText) findViewById(R.id.country_code_editText);
 
         countryRegionEditText.setCursorVisible(false);
+        countryRegionEditText.setText(countryList[0]);
         countryRegionHintEditText.setText(getString(R.string.country_region) + "  ");
+        countryCodeEditText.setText(countryCodeList[0] + "  ");
 
         countryRegionEditText.setOnClickListener(view -> {
+            AlertDialog.Builder singleChoiceDialog =
+                    new AlertDialog.Builder(SignUpActivity.this);
+            singleChoiceDialog.setTitle("请选择手机号所在国家或地区");
 
+            if (countryChoice == -1) {
+                singleChoiceDialog.setSingleChoiceItems(countryList, 0, (dialog, which) -> countryChoice = which);
+            } else {
+                singleChoiceDialog.setSingleChoiceItems(countryList, countryChoice, (dialog, which) -> countryChoice = which);
+            }
+
+            singleChoiceDialog.setPositiveButton("确定",
+                    (dialog, which) -> {
+                        if (countryChoice != -1) {
+                            countryRegionEditText.setText(countryList[countryChoice]);
+                            countryCodeEditText.setText(countryCodeList[countryChoice] + "  ");
+                        }
+                    });
+            singleChoiceDialog.show();
         });
 
         phoneNumberEditText.setOnClickListener(view -> {

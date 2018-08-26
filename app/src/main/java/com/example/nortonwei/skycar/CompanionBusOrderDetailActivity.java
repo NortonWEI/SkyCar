a5ae51c7d69e80a8526e4eb240d2da2ecac965db
Wenzhou Wei <wenzhouw@student.unimelb.edu.au>
2018-08-26 18:16:56 +1000

    order detail view with maps

--- app/src/main/java/com/example/nortonwei/skycar/CompanionBusOrderDetailActivity.java
+++ app/src/main/java/com/example/nortonwei/skycar/CompanionBusOrderDetailActivity.java
@@ -1,15 +1,42 @@
 package com.example.nortonwei.skycar;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.nortonwei.skycar.Adapter.CompanionBusSearchResultAdapter;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.Task;
 
-public class CompanionBusOrderDetailActivity extends AppCompatActivity {
+public class CompanionBusOrderDetailActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_CODE = 1234;
+    private static final float DEFAULT_ZOOM = 15f;
+
+    private boolean locationPermissionGranted = false;
+    private GoogleMap googleMap;
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +45,10 @@ public class CompanionBusOrderDetailActivity extends AppCompatActivity {
 
         setUpActionBar();
         setUpUIComponents();
+
+        if (isServiceOk()) {
+            getLocationPermission();
+        }
     }
 
     @Override
@@ -45,7 +76,99 @@ public class CompanionBusOrderDetailActivity extends AppCompatActivity {
     }
 
     private void setUpUIComponents() {
+        Button joinButton = (Button) findViewById(R.id.join_button);
+        joinButton.setOnClickListener(view -> {
 
+        });
+    }
+
+    private void setUpMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.order_detail_map);
+        mapFragment.getMapAsync(CompanionBusOrderDetailActivity.this);
+    }
+
+    private boolean isServiceOk() {
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(CompanionBusOrderDetailActivity.this);
+
+        if (available == ConnectionResult.SUCCESS) {
+            Toast.makeText(CompanionBusOrderDetailActivity.this, "Google Play Services is working", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
+            GoogleApiAvailability.getInstance().getErrorDialog(CompanionBusOrderDetailActivity.this, available, ERROR_DIALOG_REQUEST).show();
+        } else {
+            Toast.makeText(CompanionBusOrderDetailActivity.this, "You can't make requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    private void getLocationPermission() {
+        String[] permissionList = {FINE_LOCATION, COARSE_LOCATION};
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(), COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                locationPermissionGranted = true;
+                setUpMap();
+            } else {
+                ActivityCompat.requestPermissions(this, permissionList, LOCATION_PERMISSION_CODE);
+            }
+        } else {
+            ActivityCompat.requestPermissions(this, permissionList, LOCATION_PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        locationPermissionGranted = false;
+
+        switch (requestCode) {
+            case LOCATION_PERMISSION_CODE:
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                            locationPermissionGranted = false;
+                            return;
+                        }
+                    }
+                    locationPermissionGranted = true;
+                    setUpMap();
+                }
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+
+        if (locationPermissionGranted) {
+            getDeviceLocation();
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+//            googleMap.setMyLocationEnabled(true);
+        }
+    }
+
+    private void getDeviceLocation() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (locationPermissionGranted) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            Task location = fusedLocationProviderClient.getLastLocation();
+            location.addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    Location currentLocation = (Location) task.getResult();
+                    moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), DEFAULT_ZOOM);
+                } else {
+                    Toast.makeText(CompanionBusOrderDetailActivity.this, "Unable to get your current location", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    private void moveCamera(LatLng latLng, float zoom) {
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
     }
 
     public static Intent makeIntent(Context context) {

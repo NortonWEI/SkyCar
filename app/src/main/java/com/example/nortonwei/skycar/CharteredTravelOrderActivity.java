300eab922fbf786a6c4697397a1df67c6b49b3a3
Wenzhou Wei <wenzhouw@student.unimelb.edu.au>
2018-09-12 17:56:28 +1000

    chartered order view layout

--- app/src/main/java/com/example/nortonwei/skycar/CharteredTravelOrderActivity.java
+++ app/src/main/java/com/example/nortonwei/skycar/CharteredTravelOrderActivity.java
@@ -3,9 +3,11 @@ package com.example.nortonwei.skycar;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class CharteredTravelOrderActivity extends AppCompatActivity {
 
@@ -15,6 +17,7 @@ public class CharteredTravelOrderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chartered_travel_order);
 
         setUpActionBar();
+        setUpUIComponents();
     }
 
     @Override
@@ -39,6 +42,14 @@ public class CharteredTravelOrderActivity extends AppCompatActivity {
         final Drawable upArrow = getResources().getDrawable(R.drawable.abc_ic_ab_back_material);
         getSupportActionBar().setHomeAsUpIndicator(upArrow);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void setUpUIComponents() {
+        ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.title_constraintLayout);
+        TextView shareFeeTextView = (TextView) findViewById(R.id.share_fee_textView);
+        TextView shareFeeInfoTextView = (TextView) findViewById(R.id.share_fee_info_textView);
+        layout.removeView(shareFeeTextView);
+        layout.removeView(shareFeeInfoTextView);
     }
 
     public static Intent makeIntent(Context context) {
